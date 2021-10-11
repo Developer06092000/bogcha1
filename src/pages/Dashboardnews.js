a250b633a20c3e393e2476aa641d27363c8d7b7f
abbassiriyus <86982819+abbassiriyus@users.noreply.github.com>
2021-10-11 22:38:49 +0500

    erk
    e;lr

--- src/pages/Dashboardnews.js
+++ src/pages/Dashboardnews.js
@@ -52,21 +52,72 @@ export default class Dashboardnews extends Component {
 <div className={styles.cards} >
   <div className={styles.card} style={{marginTop:"40px"}}>
     <h2 className={styles.cardtitle}>Prezident tashrifi <br/>  <div style={{margin:'auto', fontSize:"10px",marginRight:'10px'}}> 14/08/2020 </div></h2>
-    <img src="https://images.unsplash.com/photo-1591485423007-765bde4139ef?ixlib=rb-1.2.1ixid=eyJhcHBfaWQiOjEyMDd9auto=formatfit=cropw=800q=80" alt=""/>
-    <p className={styles.carddesc}>Pinnipeds, commonly known as seals,[a] are a widely distributed and diverse clade of carnivorous, fin-footed, semiaquatic marine mammals. They comprise the extant families Odobenidae (whose only living member is the walrus), Otariidae (the eared seals: sea lions and fur seals), and Phocidae (the earless seals, or true seals).</p>
+    <img src={news1}  alt=""/>
+    <p className={styles.carddesc}>Pinnipeds, commonly known as seals,[a] are a widely distributed and diverse clade of carnivorous, fin-footed, semiaquatic marine mammals. They comprise the extant families Odobenidae (whose only living member is the walrus), Otariidae (the eared seals: sea lions and fur seals), and Phocidae (the earless seals, or true seals).  <p>
+  
+                          </p>
+                          <Link to="/yangilik/uz">
+                            <p
+                              style={{
+                                color: "white",
+                                backgroundColor: "blue",
+                                width: "70px",
+                                padding: "3px 10px",
+                                border: "1px solid blue",
+                                borderRadius: "10px",
+                                position:'absolute',
+                                right:'20px',
+                                bottom:'8px'
+                              }}
+                            >
+                              Batafsil
+                            </p>
+                          </Link></p>
   </div>
 
   <div className={styles.card} style={{marginTop:"40px"}}>
     <h2 className={styles.cardtitle}>Prezident tashrifi <br/> <div style={{margin:'auto', fontSize:"10px",marginRight:'10px'}}>14/08/2020 </div></h2>
-    <img src="https://images.unsplash.com/photo-1534188753412-3e26d0d618d6?ixlib=rb-1.2.1ixid=eyJhcHBfaWQiOjEyMDd9auto=formatfit=cropw=634q=80" alt=""/>
-    <p className={styles.carddesc}>The lion (Panthera leo) is a species in the family Felidae and a member of the genus Panthera. It has a muscular, deep-chested body, short, rounded head, round ears, and a hairy tuft at the end of its tail. It is sexually dimorphic; adult male lions have a prominent mane.</p>
+    <img src={news1}  alt=""/>
+    <p className={styles.carddesc}>The lion (Panthera leo) is a species in the family Felidae and a member of the genus Panthera. It has a muscular, deep-chested body, short, rounded head, round ears, and a hairy tuft at the end of its tail. It is sexually dimorphic; adult male lions have a prominent mane.  <Link to="/yangilik/uz">
+                            <p
+                              style={{
+                                color: "white",
+                                backgroundColor: "blue",
+                                width: "70px",
+                                padding: "3px 10px",
+                                border: "1px solid blue",
+                                borderRadius: "10px",
+                                position:'absolute',
+                                right:'20px',
+                                bottom:'8px'
+                              }}
+                            >
+                              Batafsil
+                            </p>
+                          </Link></p>
   </div>
 
   <div className={styles.card} style={{marginTop:'40px'}}>
     <h2 className={styles.cardtitle}>Prezident tashrifi <br/>   <div style={{margin:'auto', fontSize:"10px",marginRight:'10px'}}>14/08/2020 </div></h2>
     <img  src={news1} alt=""/>
     <p className={styles.carddesc}>Hawks are a group of medium-sized diurnal birds of prey of the family Accipitridae. Hawks are widely distributed and vary greatly in size.
-      The subfamily Accipitrinae includes goshawks, sparrowhawks, sharp-shinned hawks and others. This subfamily are mainly woodland birds with long tails and high visual acuity. They hunt by dashing suddenly from a concealed perch.</p>
+      The subfamily Accipitrinae includes goshawks, sparrowhawks, sharp-shinned hawks and others. This subfamily are mainly woodland birds with long tails and high visual acuity. They hunt by dashing suddenly from a concealed perch.  <Link to="/yangilik/uz">
+                            <p
+                              style={{
+                                color: "white",
+                                backgroundColor: "blue",
+                                width: "70px",
+                                padding: "3px 10px",
+                                border: "1px solid blue",
+                                borderRadius: "10px",
+                                position:'absolute',
+                                right:'20px',
+                                bottom:'8px'
+                              }}
+                            >
+                              Batafsil
+                            </p>
+                          </Link></p>
   </div>
 </div>
 
